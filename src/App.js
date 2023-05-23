4e27e064999065e58469c4748d2cf893baad8e15
nauval01~ <nauvalmulyana242@gmail.com>
2023-05-23 13:20:21 +0700

    push fe

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import UserList from "./components/UserList";
+import AddUSer from "./components/AddUSer";
+import EditUSer from "./components/EditUser";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<UserList />} />
+        <Route path="add" element={<AddUSer />} />
+        <Route path="edit/:id" element={<EditUSer />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
